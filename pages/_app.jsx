216236f5f951fce2a8b99791a0ab1bd27ac72229
bx huang <huozaixianshi123@gmail.com>
2022-08-17 18:01:19 +0800

    alchemyweek4

---
+++ pages/_app.jsx
@@ -0,0 +1,13 @@
+/*
+ * @Description: 
+ * @Author: startha
+ * @Date: 2022-08-17 17:27:21
+ * @LastEditTime: 2022-08-17 17:31:54
+ * @LastEditors: startha
+ * @Reference: 
+ */
+import '../styles/globals.css'
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+export default MyApp
